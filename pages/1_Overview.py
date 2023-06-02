da7eb91b68d1c2a455fdc094585aa7362d0fa413
P-Heckmann <paul.he@posteo.de>
2023-06-02 11:30:17 +0200

    c

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -3,7 +3,7 @@ import pandas as pd
 import altair as alt
 
 
-st.write("# Welcome to the Billboard analyser")
+
 
 path = "./data/merged_df.pkl"
 
@@ -17,13 +17,18 @@ df = pd.read_pickle(path_local)
 
 #df.describe()
 
+st.write("### Welcome to the Billboard analyser")
+st.markdown("""---""")
+
+NUMBER_OF_ENTRIES = len(df)
+
+
 chart = alt.Chart(df).mark_bar().encode(
     alt.X('year:O', bin=alt.Bin(step=1), title='Year'),
     alt.Y('count()', title='Frequency')
-).properties(
-    title='Songs per year'
-)
-chart
+).interactive()
+
+st.altair_chart(chart, use_container_width=True)
 
 # Define a dictionary to map numerical data to string data
 mapping = {0: 'C',
@@ -45,6 +50,8 @@ df['key'] = df['key'].replace(mapping)
 
 df['duration_m'] = df['duration_ms'] / 60000
 
+df['time_signature'] = df['time_signature'].astype(int)
+
 st.markdown("""---""")
 st.write(f"#### Key")
 
@@ -52,7 +59,7 @@ chart = alt.Chart(df).mark_bar().encode(
     alt.X('key:O', title='Key'),
     alt.Y('count()', title='Frequency')
 ).interactive()
-chart
+
 st.altair_chart(chart, use_container_width=True)
 
 
@@ -61,27 +68,43 @@ st.write(f"#### Tempo")
 
 x_range = (50,220)
 chart = alt.Chart(df).mark_bar().encode(
-    alt.X('tempo',bin=alt.Bin(step=10),scale=alt.Scale(domain=x_range),  title='tempo (bpm)'),
+    alt.X('tempo',bin=alt.Bin(step=10),scale=alt.Scale(domain=x_range),  title='Beats per minute'),
     alt.Y('count()', title='Frequency')
 ).interactive()
 
-chart
+
 st.altair_chart(chart, use_container_width=True)
 
+
+
 metrics = ["danceability","speechiness","instrumentalness",
-           "liveness","valence","duration_m","time_signature"
-           ]
+           "liveness","valence"]
 
-for metric in metrics():
-    st.markdown("""---""")
-    st.write(f"#### {metric}")
+metrics_name = ["Danceability","Speechiness","Instrumentalness",
+           "Liveness","Valence"]
 
+
+
+
+for metric, name in zip(metrics,metrics_name) :
+    st.write(f"#### {name}")
+    
     chart = alt.Chart(df).mark_bar().encode(
-        alt.X(metric, bin=alt.Bin(step=0.1),  title='tempo (bpm)'),
-        alt.Y('count()', title='Frequency')
+    alt.X(metric,bin=alt.Bin(step=0.10), title=name),
+    alt.Y('count()', title='Frequency')
     ).interactive()
-    chart
     st.altair_chart(chart, use_container_width=True)
 
+st.markdown("""---""")
+st.write(f"#### Duration in minutes")
 
+x_range2 = (1,8)
+
+chart = alt.Chart(df).mark_bar().encode(
+    alt.X('duration_m', bin=alt.Bin(step=1),scale=alt.Scale(domain=x_range2), title='Minutes'),
+    alt.Y('count()', title='Frequency')
+).interactive()
+st.altair_chart(chart, use_container_width=True)
+
+    
 

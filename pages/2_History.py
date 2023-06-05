526c48ece4ee087da4292bb35017a9724f7d57c5
P-Heckmann <paul.he@posteo.de>
2023-06-05 10:50:36 +0200

    c

--- pages/2_History.py
+++ pages/2_History.py
@@ -38,61 +38,34 @@ mapping = {0: 'C',
 df['key'] = df['key'].replace(mapping)
 
 
-# Group the DataFrame by 'Name' and 'Year' and count the occurrences
-counts = df.groupby(['key', 'year']).size().reset_index(name='Count')
+metrics = ["key", "tempo", "danceability","speechiness","instrumentalness",
+           "liveness","valence"]
 
+selected_metric = st.selectbox('Select a metric:', metrics)
+
+
+
+# Group the DataFrame by 'Name' and 'Year' and count the occurrences
+counts = df.groupby([selected_metric, 'year']).size().reset_index(name='Count')
 # Pivot the counts DataFrame to have names as columns and years as index
 pivoted = counts.pivot(index='year', columns='key', values='Count')
-
 pivoted = pivoted.reset_index(drop=False)
 #df = pivoted.reset_index()
-
-
-
-
 # Melt the data to long format
 melted_data = pd.melt(pivoted, id_vars='year', var_name='Key', value_name='Count')
 melted_data = melted_data.dropna()
-
 # Create the stacked bar plot using Altair
 chart = alt.Chart(melted_data).mark_bar().encode(
     x='year:O',
     y='Count:Q',
     color='Key:N'
 ).interactive()
-#chart
+
 st.altair_chart(chart, use_container_width=True)
-
-
 melted_data['Normalized_count'] = melted_data.groupby('year')['Count'].apply(lambda x: x / x.sum())
-
-
-
 chart = alt.Chart(melted_data).mark_bar().encode(
     x='year:O',
     y='Normalized_count:Q',
     color='Key:N'
 ).interactive()
-
 st.altair_chart(chart, use_container_width=True)
-
-# Normalize column1 from 0 to 1
-#normalized_counts = (melted_data['Count'] - melted_data['Count'].min()) / (melted_data['Count'].max() - melted_data['Count'].min())
-
-# Replace the column1 values with the normalized values
-#melted_data['Normed'] = normalized_counts
-
-
-# group the dataframe by year and normalize the count column
-counts['normalized_count'] = counts.groupby('year')['Count'].apply(lambda x: x / x.sum())
-
-# Pivot the counts DataFrame to have names as columns and years as index
-pivoted_normalized = counts.pivot(index='year', columns='key', values='normalized_count')
-
-# Create a stacked bar chart using the pivoted DataFrame
-pivoted_normalized.plot(kind='bar', stacked=True, figsize=(15,6))
-
-plt.ylabel('Count')
-plt.xlabel('')
-# Move the legend outside the figure
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')

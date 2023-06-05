7ec601aeede8a1a3027441641274641e32289ed4
P-Heckmann <paul.he@posteo.de>
2023-06-05 09:39:07 +0200

    c

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -21,6 +21,20 @@ NUMBER_OF_ENTRIES = len(df)
 FIRST_YEAR = df["year"].min()
 LAST_YEAR = 2021
 
+DANCEABLITY = '"Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."'
+
+SPEECHINESS = '"Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks."'
+
+INSTRUMENTALNESS = '"Predicts whether a track contains no vocals. "Ooh" and "aah" sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly "vocal". The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0."'
+
+LIVENESS = '"Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live."'
+
+VALENCE = '"A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry)."'
+
+
+
+
+
 st.write(f"#### Number of songs: {NUMBER_OF_ENTRIES}")
 st.write(f"#### From {FIRST_YEAR} to {LAST_YEAR}")
 st.markdown("""---""") 
@@ -85,17 +99,19 @@ metrics = ["danceability","speechiness","instrumentalness",
 metrics_name = ["Danceability","Speechiness","Instrumentalness",
            "Liveness","Valence"]
 
+info_list = [DANCEABLITY,SPEECHINESS, INSTRUMENTALNESS, LIVENESS, VALENCE]
 
 
-
-for metric, name in zip(metrics,metrics_name) :
+for metric, name, info in zip(metrics,metrics_name,info_list) :
     st.write(f"#### {name}")
+    st.write(f"##### {info}")
     
     chart = alt.Chart(df).mark_bar().encode(
     alt.X(metric,bin=alt.Bin(step=0.10), title=name),
     alt.Y('count()', title='Frequency')
     ).interactive()
     st.altair_chart(chart, use_container_width=True)
+    
 
 st.markdown("""---""")
 st.write(f"#### Duration in minutes")

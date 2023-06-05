b634f7af01faaaddb22fd890ff67b98b4476e3b9
P-Heckmann <paul.he@posteo.de>
2023-06-05 11:27:21 +0200

    c

--- pages/2_History.py
+++ pages/2_History.py
@@ -38,34 +38,74 @@ mapping = {0: 'C',
 df['key'] = df['key'].replace(mapping)
 
 
-metrics = ["key", "tempo", "danceability","speechiness","instrumentalness",
-           "liveness","valence"]
+
+
+# Define the bin edges
+tempo_bins = [0,80,90,100,110,120,130,140,160,300]
+
+# Define the bin labels
+labels = ['0-80 BPM', '80-90 BPM',
+          '90-100 BPM', '100-110 BPM', 
+          '110-120 BPM ', '120-130 BPM',
+          '130-140 BPM', '140-160 BPM', '160-250 BPM']
+
+
+df['Tempo'] = pd.cut(df['tempo'], bins=tempo_bins, labels=labels)
+
+
+metrics = ["key", "tempo" ] #, "danceability","speechiness","instrumentalness", "liveness","valence"]
 
 selected_metric = st.selectbox('Select a metric:', metrics)
-
 
 
 # Group the DataFrame by 'Name' and 'Year' and count the occurrences
 counts = df.groupby([selected_metric, 'year']).size().reset_index(name='Count')
 # Pivot the counts DataFrame to have names as columns and years as index
-pivoted = counts.pivot(index='year', columns='key', values='Count')
+pivoted = counts.pivot(index='year', columns=selected_metric, values='Count')
 pivoted = pivoted.reset_index(drop=False)
 #df = pivoted.reset_index()
 # Melt the data to long format
-melted_data = pd.melt(pivoted, id_vars='year', var_name='Key', value_name='Count')
+
+
+melted_data = pd.melt(pivoted, id_vars='year', var_name=selected_metric, value_name='Count')
 melted_data = melted_data.dropna()
+
+
+
 # Create the stacked bar plot using Altair
 chart = alt.Chart(melted_data).mark_bar().encode(
     x='year:O',
     y='Count:Q',
-    color='Key:N'
+    color='selected_metric:N'
 ).interactive()
 
+# Specify the order of the legend
+#custom_sort_order = labels
+#chart = chart.encode(
+#    color=alt.Color('Tempo:N', sort=custom_sort_order, legend=alt.Legend(title='Tempo')),
+#)
+
+#chart
+
+
 st.altair_chart(chart, use_container_width=True)
+
 melted_data['Normalized_count'] = melted_data.groupby('year')['Count'].apply(lambda x: x / x.sum())
 chart = alt.Chart(melted_data).mark_bar().encode(
     x='year:O',
     y='Normalized_count:Q',
-    color='Key:N'
+    color='Tempo:N'
 ).interactive()
+
+# Specify the order of the legend
+#custom_sort_order = labels
+#chart = chart.encode(
+#    color=alt.Color('Tempo:N', sort=custom_sort_order, legend=alt.Legend(title='Tempo')),
+#)
+
+
+#chart
+
+
+
 st.altair_chart(chart, use_container_width=True)

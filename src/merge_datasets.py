3316b47561eb6b6019d6b8a60d3aab32b77d35b2
P-Heckmann <paul.he@posteo.de>
2023-06-06 13:57:04 +0200

    c

--- src/merge_datasets.py
+++ src/merge_datasets.py
@@ -2,46 +2,45 @@ import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# Set up the client credentials
+# Set up the client credentials for Spotify API authentication
 client_credentials_manager = SpotifyClientCredentials(
     client_id="54b3ffcb9b85494aa3ca79a540013c94",
     client_secret="68d913a2f09f4086ab7ab1244816d99b",
 )
+
+# Create a Spotify API client
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-
+# Read the DataFrame from a pickle file
 df = pd.read_pickle("./data/songs.pkl")
-
 
 # Define a function to get audio features for a list of track URIs
 def get_audio_features(uris):
     features_list = []
     for uri in uris:
+        # Get the audio features for each track URI
         features = sp.audio_features(uri)
         features_list.append(features[0])
     return features_list
 
-
-# Define a DataFrame with track URIs
-df = pd.read_pickle("./data/songs.pkl")
-
-# Call the function to get audio features for the tracks
+# Call the get_audio_features function to retrieve audio features for the track URIs in the DataFrame
 audio_features = get_audio_features(df["spotify_uri"].tolist())
 
-# filter out invalid entry
+# Filter out any invalid entries from the audio features list
 data = [i for i in audio_features if i is not None]
 
 # Convert the list of audio features into a DataFrame
 audio_features_df = pd.DataFrame(data)
 
+# Rename the "uri" column to "spotify_uri"
 audio_features_df = audio_features_df.rename(columns={"uri": "spotify_uri"})
 
-# Merge the original DataFrame with the audio features DataFrame
+# Merge the original DataFrame with the audio features DataFrame based on the "spotify_uri" column
 merged_df = pd.merge(df, audio_features_df, on="spotify_uri", how="left")
 
-
-
+# Drop any rows with missing values
 merged_df = merged_df.dropna()
 
-merged_df.to_pickle("./data/merged_df.pkl")
+# Save the merged DataFrame as a pickle file named "merged_df.pkl"
+merged_df.to_pickle(r"./data/merged_df.pkl")
 
--- src/wiki_scraping.py
+++ src/wiki_scraping.py
@@ -1,17 +1,21 @@
 import pandas as pd
-import numpy as np
 
+# Define a list to store the URLs of Wikipedia pages
 urls = []
 for year in range(1958, 2023):
     urls.append(
         f"https://en.wikipedia.org/wiki/List_of_Billboard_Hot_100_top-ten_singles_in_{year}"
     )
 
+# Create an empty list to store rows of data
 rows = []
 for url, year in zip(urls, range(1958, 2023)):
     print(year)
+    
+    # Read HTML tables from the Wikipedia page
     dfs = pd.read_html(url)
 
+    # Find the table with the maximum number of rows
     row_num = []
     for df in dfs:
         row_num.append(df.shape[0])
@@ -19,21 +23,31 @@ for url, year in zip(urls, range(1958, 2023)):
     year_df = dfs[row_num.index(max(row_num))]
     year_df = year_df.iloc[:, :6]
 
+    # Rename the columns of the selected table
     columns = ["entry_date", "title", "artist", "peak", "peak_date", "weeks_top_ten"]
     year_df.columns = columns
 
+    # Filter out rows where the "peak" column cannot be converted to numeric values
     year_df = year_df[
         ~pd.to_numeric(year_df["peak"], errors="coerce").isna()
     ].reset_index(drop=True)
+
+    # Add a new column for the year
     year_df["year"] = year
+    
+    # Append the rows of the selected table to the list
     rows.extend(year_df.to_dict(orient="records"))
 
+# Create a DataFrame from the list of rows
 df = pd.DataFrame(rows)
 
+# Define a list of characters to be replaced in the "title" column
 to_replace = ["/", "\\\\", "(", ")", "'", ":", "."]
 
+# Replace characters in the "title" column
 for item in to_replace:
     df["title"] = df["title"].str.replace(item, "", regex=False)
 
+# Save the DataFrame as a pickle file named "hot_100.pkl"
+df.to_pickle(r"./data/hot_100.pkl")
 
-df.to_pickle(r"C:\Users\paulh\Desktop\Data Science Projects\data\hot_100.pkl")

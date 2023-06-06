5e1313a45d50269ba9bd27d4f841f2b2ec55cc07
P-Heckmann <paul.he@posteo.de>
2023-06-06 13:57:02 +0200

    c

--- src/get_spotify_uri.py
+++ src/get_spotify_uri.py
@@ -4,32 +4,45 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
 
+# Set the required environment variables for Spotify API authentication
 os.environ["SPOTIPY_CLIENT_ID"] = "54b3ffcb9b85494aa3ca79a540013c94"
 os.environ["SPOTIPY_CLIENT_SECRET"] = "68d913a2f09f4086ab7ab1244816d99b"
 os.environ["SPOTIPY_REDIRECT_URI"] = "http://localhost:8080"
 
-
+# Initialize the Spotify API client
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
+# Read the pickle file "hot_100.pkl" into a DataFrame
 df = pd.read_pickle(".\data\hot_100.pkl")
 
+# Convert the DataFrame to a list of dictionaries
 list_df = df.to_dict(orient="records")
 
+# Iterate over each item in the list and query the Spotify API for track information
 for item in list_df:
     try:
+        # Search for a track based on the item's title and artist using the Spotify API
         result = spotify.search(
             f"""track:{item['title']} artist:{item['artist']}""", type="track", limit=1
         )
         if len(result["tracks"]["items"]) > 0:
+            # Store the URI of the first track in the search result
             item["spotify_uri"] = result["tracks"]["items"][0]["uri"]
         else:
+            # If no track is found, set the URI as NaN
             item["spotify_uri"] = np.nan
     except spotipy.client.SpotifyException as e:
+        # If an exception occurs, store the exception details in the URI
         item["spotify_uri"] = str(e.http_status) + " - " + e.msg
 
+# Create a new DataFrame from the updated list of dictionaries
 df = pd.DataFrame(list_df)
 
+# Drop rows with NaN values in the "spotify_uri" column
 df = df.dropna()
+
+# Filter out rows where the "spotify_uri" column contains the string "Not found"
 df = df[~df["spotify_uri"].str.contains("Not found")]
 
-df.to_pickle(r"C:\Users\paulh\Desktop\Data Science Projects\data\songs.pkl")
+# Save the DataFrame as a pickle file named "songs.pkl"
+df.to_pickle(r"./data/songs.pkl")
